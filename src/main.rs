1d4355ec0a4812fa6f386364d2b6277ef676f053
Jonathan Lalande <lalandej.gg@gmail.com>
2022-12-25 05:28:32 -0500

    Fix address generation
    Use byte representation of public key instead of its string representation.

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,31 @@
 use openssl::nid::Nid;
 use openssl::bn::{BigNum, BigNumContext};
-use openssl::ec::{EcGroup, EcKey};
+use openssl::ec::{EcGroup, EcKey, PointConversionForm};
 use openssl::hash::{hash, MessageDigest};
 
 fn main() {
     let mut ctx = BigNumContext::new().unwrap();
     let curve_nid = Nid::SECP256K1;
     let group = EcGroup::from_curve_name(curve_nid).unwrap();
-    let key = EcKey::generate(&group).unwrap();
 
+    let key = EcKey::generate(&group).unwrap();
     let private_key = key.private_key();
-    let public_key = &key.public_key();
+    let public_key = key.public_key();
+
     let mut pub_x = BigNum::new().unwrap();
     let mut pub_y = BigNum::new().unwrap();
     public_key
         .affine_coordinates(&group.as_ref(), &mut pub_x, &mut pub_y, &mut ctx)
         .unwrap();
 
+    let public_key_bytes = public_key
+        .to_bytes(&group, PointConversionForm::UNCOMPRESSED, &mut ctx)
+        .unwrap();
+
     let private_key_str = private_key.to_hex_str().unwrap();
     let public_key_str = format!("04{}{}", pub_x.to_hex_str().unwrap(), pub_y.to_hex_str().unwrap());
 
-    println!("Rcoin address: {}", generate_address(public_key_str.as_str()));
+    println!("Rcoin address: {}", generate_address(&public_key_bytes));
     println!("==================================");
     println!("private key:\t{}", private_key_str);
     println!("public key:\t{}", public_key_str);
@@ -32,9 +37,8 @@ fn main() {
 //
 // Addresses generation follows the same rules as Bitcoin:
 // https://github.com/bitcoinbook/bitcoinbook/blob/develop/ch04.asciidoc#bitcoin-addresses
-fn generate_address(public_key: &str) -> String {
-    let payload = public_key.as_bytes();
-    let payload = hash(MessageDigest::sha256(), payload).unwrap();
+fn generate_address(public_key: &[u8]) -> String {
+    let payload = hash(MessageDigest::sha256(), public_key).unwrap();
     let payload = hash(MessageDigest::ripemd160(), &payload).unwrap();
     let mut payload = payload.to_vec();
 
