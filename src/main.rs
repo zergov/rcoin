dd5b7749b73c333cf8f6e0df93de6c5901ea97eb
Jonathan Lalande <lalandej.gg@gmail.com>
2022-12-25 04:52:40 -0500

    Add function to generate Rcoin address

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use openssl::nid::Nid;
 use openssl::bn::{BigNum, BigNumContext};
 use openssl::ec::{EcGroup, EcKey};
+use openssl::hash::{hash, MessageDigest};
 
 fn main() {
     let mut ctx = BigNumContext::new().unwrap();
@@ -26,4 +27,33 @@ fn main() {
     println!("x:\t{}", pub_x.to_hex_str().unwrap());
     println!("y:\t{}", pub_y.to_hex_str().unwrap());
     println!("k:\t{}", public_key_str);
+    println!("");
+    println!("address ====================");
+    println!("addr: {}", generate_address(public_key_str.as_str()))
+}
+
+// Public key to Rcoin address.
+//
+// Addresses generation follows the same rules as Bitcoin:
+// https://github.com/bitcoinbook/bitcoinbook/blob/develop/ch04.asciidoc#bitcoin-addresses
+fn generate_address(public_key: &str) -> String {
+    let payload = public_key.as_bytes();
+    let payload = hash(MessageDigest::sha256(), payload).unwrap();
+    let payload = hash(MessageDigest::ripemd160(), &payload).unwrap();
+    let mut payload = payload.to_vec();
+
+    // Base58Check prefix version: 0x00 for addresses.
+    payload.insert(0, 0x00);
+
+    let checksum = hash(MessageDigest::sha256(), &payload).unwrap();
+    let checksum = hash(MessageDigest::sha256(), &checksum).unwrap();
+    let mut checksum = checksum
+        .to_vec()
+        .into_iter()
+        .take(4)
+        .collect();
+
+    payload.append(&mut checksum);
+
+    bs58::encode(payload).into_string()
 }
