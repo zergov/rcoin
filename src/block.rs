600aa6591359ae19f16aa3a1ec88f629ddc6e7f7
Jonathan Lalande <lalandej.gg@gmail.com>
2022-12-28 14:44:05 -0500

    use u256 in block.rs

--- src/block.rs
+++ src/block.rs
@@ -1,3 +1,4 @@
+use ethnum::{U256, u256};
 use openssl::hash::{hash, MessageDigest};
 
 pub struct Block {
@@ -6,21 +7,19 @@ pub struct Block {
 
 pub struct Header {
     pub version: u32,
-    pub prev_block_hash: [u8; 32],
-    pub merkle_root: [u8; 32],
+    pub prev_block_hash: u256,
+    pub merkle_root: u256,
     pub time: u32,
     pub bits: u32,
     pub nounce: u32,
 }
 
 pub fn genesis() -> Block {
-    let merkle_root = hex::decode("4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b").unwrap();
-
     Block {
         header: Header{
             version: 0x1,
-            prev_block_hash: [0; 32],
-            merkle_root: merkle_root.try_into().unwrap(),
+            prev_block_hash: U256::new(0),
+            merkle_root: U256::from_str_radix("4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b", 16).unwrap(),
             time: 1231006505,
             bits: 486604799,
             nounce: 2083236893,
@@ -29,17 +28,12 @@ pub fn genesis() -> Block {
 }
 
 impl Block {
-    pub fn hash(&self) -> [u8; 32] {
+    pub fn hash(&self) -> u256 {
         let mut payload: Vec<u8> = Vec::new();
 
-        let mut prev_block_hash = self.header.prev_block_hash.clone();
-        let mut merkle_root = self.header.merkle_root.clone();
-        prev_block_hash.reverse();
-        merkle_root.reverse();
-
         payload.extend(self.header.version.to_le_bytes());
-        payload.extend(prev_block_hash);
-        payload.extend(merkle_root);
+        payload.extend(self.header.prev_block_hash.to_le_bytes());
+        payload.extend(self.header.merkle_root.to_le_bytes());
         payload.extend(self.header.time.to_le_bytes());
         payload.extend(self.header.bits.to_le_bytes());
         payload.extend(self.header.nounce.to_le_bytes());
@@ -47,9 +41,8 @@ impl Block {
         let payload = hash(MessageDigest::sha256(), &payload).unwrap();
         let payload = hash(MessageDigest::sha256(), &payload).unwrap();
 
-        let mut payload = payload.to_vec();
-        payload.reverse();
-        payload.try_into().unwrap()
+        let payload: [u8; 32] = payload.to_vec().try_into().unwrap();
+        U256::from_le_bytes(payload)
     }
 }
 
@@ -62,9 +55,9 @@ mod test {
         let header = genesis().header;
 
         assert_eq!(0x1, header.version);
-        assert_eq!([0x0; 32], header.prev_block_hash);
+        assert_eq!(0, header.prev_block_hash);
         assert_eq!(
-            hex::decode("4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b").unwrap(),
+            U256::from_str_radix("4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b", 16).unwrap(),
             header.merkle_root
         );
         assert_eq!(1231006505, header.time);
@@ -75,8 +68,8 @@ mod test {
     #[test]
     fn genesis_hash_test() {
         assert_eq!(
-            "000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f",
-            hex::encode(genesis().hash().to_vec())
+            U256::from_str_radix("000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f", 16).unwrap(),
+            genesis().hash()
         );
     }
 }
