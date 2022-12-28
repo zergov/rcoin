270d871fd16c41c22ed05b576604a0828d23e34e
Jonathan Lalande <lalandej.gg@gmail.com>
2022-12-27 23:59:30 -0500

    Init CLI app in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,51 @@
-fn main() {
-    let keychain = rcoin::keys::generate_keychain();
+use clap::{Args, Parser, Subcommand};
 
-    println!("Rcoin address: {}", rcoin::addresses::from_keychain(&keychain));
-    println!("==================================");
-    println!("private key:\t{}", keychain.private_key_hex());
-    println!("public key:\t{}", keychain.public_key_hex());
+#[derive(Parser)]
+#[command(name = "rcoin")]
+#[command(author = "Jonathan Lalande. <lalandej.gg@gmail.com>")]
+#[command(version = "0.0")]
+#[command(about = "client to interact with the rcoin network.", long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    commands: Commands
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Wallet(Wallet)
+}
+
+#[derive(Args)]
+struct Wallet {
+    #[command(subcommand)]
+    commands: WalletCommands,
+}
+
+#[derive(Subcommand)]
+enum WalletCommands {
+    /// Generate a new rcoin wallet at the given path.
+    New { path: String }
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    match &cli.commands {
+        Commands::Wallet(wallet) => wallet_command(&wallet)
+    }
+
+}
+
+fn wallet_command(wallet: &Wallet) {
+    match &wallet.commands {
+        WalletCommands::New{ path } => {
+            println!("creating new wallet at: {}", path);
+            let keychain = rcoin::keys::generate_keychain();
+
+            println!("Rcoin address: {}", rcoin::addresses::from_keychain(&keychain));
+            println!("==================================");
+            println!("private key:\t{}", keychain.private_key_hex());
+            println!("public key:\t{}", keychain.public_key_hex());
+        }
+    }
 }
