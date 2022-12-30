c24c0a6595523cadc0d627c0c91a35ee30b5c0a9
Jonathan Lalande <lalandej.gg@gmail.com>
2022-12-30 17:56:35 -0500

    add hash160 function to hash.rs

--- src/hash.rs
+++ src/hash.rs
@@ -1,5 +1,11 @@
 use openssl::hash::{hash, MessageDigest};
 
+pub fn hash160(data: &[u8]) -> Vec<u8> {
+    let data = hash(MessageDigest::sha256(), &data).unwrap();
+    let data = hash(MessageDigest::ripemd160(), &data).unwrap();
+    data.to_vec()
+}
+
 pub fn hash256(data: &[u8]) -> Vec<u8> {
     let data = hash(MessageDigest::sha256(), &data).unwrap();
     let data = hash(MessageDigest::sha256(), &data).unwrap();
