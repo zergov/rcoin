f52f732032b0b406783c116b5ca538339148eeb4
Jonathan Lalande <lalandej.gg@gmail.com>
2022-12-31 00:53:39 -0500

    start over :)

--- src/scripts.rs
+++ src/scripts.rs
@@ -1,48 +1,20 @@
-use openssl::hash::{hash, MessageDigest};
+struct ScriptEngine {
+    script: Vec<u8>,
+    stack: Vec<Vec<u8>>,
+    ip: u32,
+}
 
-fn execute(script: String) -> Result<bool, String> {
-    let mut stack: Vec<String> = Vec::new();
-    for token in script.split_whitespace() {
-        match token {
-            "ADD" => {
-                match (stack.pop(), stack.pop()) {
-                    (Some(a), Some(b)) => {
-                        let a: u32 = a.parse().unwrap();
-                        let b: u32 = b.parse().unwrap();
-                        stack.push((a + b).to_string());
-                    },
-                    _ => return Err(String::from("ADD: missing values on stack.")),
-                }
-            },
-            "EQUAL" => {
-                match (stack.pop(), stack.pop()) {
-                    (Some(a), Some(b)) => {
-                        let out = if a == b { "1" } else { "0" };
-                        stack.push(String::from(out))
-                    },
-                    _ => return Err(String::from("EQUAL: missing values on stack.")),
-                }
-            },
-            "SHA256" => {
-                match stack.pop() {
-                    Some(value) => {
-                        let sha256 = hash(MessageDigest::sha256(), value.as_bytes()).unwrap();
-                        stack.push(hex::encode(sha256));
-                    }
-                    _ => return Err(String::from("SHA256: missing value on stack."))
-                }
-            }
-            _ => stack.push(token.to_string()),
-        };
+impl ScriptEngine {
+    pub fn new() -> ScriptEngine {
+        ScriptEngine {
+            script: vec![],
+            stack: vec![],
+            ip: 0,
+        }
     }
 
-    if stack.len() > 1 {
-        return Ok(false);
-    };
-
-    match stack.pop() {
-        None => Ok(false),
-        Some(s) => Ok(s != "0"),
+    pub fn execute(&mut self, script: Vec<u8>) -> Result<bool, String> {
+        Ok(true)
     }
 }
 
@@ -53,50 +25,54 @@ mod test {
 
     #[test]
     fn test_math_puzzle_success() {
-        let script = String::from("4 4 ADD 8 EQUAL");
-        assert_eq!(Ok(true), execute(script))
+        // 4 4 ADD 8 EQUAL
+        let script = hex::decode("0104010493010887").unwrap();
+        let mut script_engine = ScriptEngine::new();
+        assert_eq!(Ok(true), script_engine.execute(script))
     }
 
     #[test]
     fn test_math_puzzle_failure() {
-        let script = String::from("1 4 ADD 8 EQUAL");
-        assert_eq!(Ok(false), execute(script))
+        // 4 1 ADD 8 EQUAL
+        let script = hex::decode("0104010193010887").unwrap();
+        let mut script_engine = ScriptEngine::new();
+        assert_eq!(Ok(false), script_engine.execute(script))
     }
 
-    #[test]
-    fn test_math_puzzle_add_missing_stack_value() {
-        let script = String::from("4 ADD 8 EQUAL");
-        assert_eq!(Err(String::from("ADD: missing values on stack.")), execute(script))
-    }
+    // #[test]
+    // fn test_math_puzzle_add_missing_stack_value() {
+        // let script = String::from("4 ADD 8 EQUAL");
+        // assert_eq!(Err(String::from("ADD: missing values on stack.")), execute(script))
+    // }
 
-    #[test]
-    fn test_math_puzzle_equal_missing_stack_value() {
-        let script = String::from("4 4 ADD EQUAL");
-        assert_eq!(Err(String::from("EQUAL: missing values on stack.")), execute(script))
-    }
+    // #[test]
+    // fn test_math_puzzle_equal_missing_stack_value() {
+        // let script = String::from("4 4 ADD EQUAL");
+        // assert_eq!(Err(String::from("EQUAL: missing values on stack.")), execute(script))
+    // }
 
-    #[test]
-    fn test_hash_puzzle_success() {
-        let script = String::from("rcoin SHA256 660e4502ce8f393eb5d5710febc339a58778bce175e4647ce50f8639786d132a EQUAL");
-        assert_eq!(Ok(true), execute(script))
-    }
+    // #[test]
+    // fn test_hash_puzzle_success() {
+        // let script = String::from("rcoin SHA256 660e4502ce8f393eb5d5710febc339a58778bce175e4647ce50f8639786d132a EQUAL");
+        // assert_eq!(Ok(true), execute(script))
+    // }
 
-    #[test]
-    fn test_hash_puzzle_failure() {
-        let script = String::from("something_else SHA256 660e4502ce8f393eb5d5710febc339a58778bce175e4647ce50f8639786d132a EQUAL");
-        assert_eq!(Ok(false), execute(script))
-    }
+    // #[test]
+    // fn test_hash_puzzle_failure() {
+        // let script = String::from("something_else SHA256 660e4502ce8f393eb5d5710febc339a58778bce175e4647ce50f8639786d132a EQUAL");
+        // assert_eq!(Ok(false), execute(script))
+    // }
 
-    #[test]
-    fn test_sha256_missing_stack_value() {
-        let script = String::from("SHA256 660e4502ce8f393eb5d5710febc339a58778bce175e4647ce50f8639786d132a EQUAL");
-        assert_eq!(Err(String::from("SHA256: missing value on stack.")), execute(script))
-    }
+    // #[test]
+    // fn test_sha256_missing_stack_value() {
+        // let script = String::from("SHA256 660e4502ce8f393eb5d5710febc339a58778bce175e4647ce50f8639786d132a EQUAL");
+        // assert_eq!(Err(String::from("SHA256: missing value on stack.")), execute(script))
+    // }
 
-    #[test]
-    fn test_p2pk_success() {
-       let keychain = crate::keys::generate_keychain();
-       let public_key = keychain.public_key_hex();
-       let script = String::from("{} OP_CHECKSIG");
-    }
+    // #[test]
+    // fn test_p2pk_success() {
+       // let keychain = crate::keys::generate_keychain();
+       // let public_key = keychain.public_key_hex();
+       // let script = String::from("{} OP_CHECKSIG");
+    // }
 }
